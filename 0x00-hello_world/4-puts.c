c19bc0768ba8341638603e5d14dcab438e072053
islam-ahmed33 <eslamaborayah@gmail.com>
2023-02-16 08:34:59 -0800

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+/**
+* main - a program that prints a line using puts function instead of printf
+* Return: 0 (This program will be successful)
+*/
 int main(void){
   puts("\"Programming is like building a multilingual puzzle");
   return 0;
