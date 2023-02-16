69d3766916a43ada300e221cd330f0a99248cc91
islam-ahmed33 <eslamaborayah@gmail.com>
2023-02-16 08:46:40 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *return: 0 (This program will be successful)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
