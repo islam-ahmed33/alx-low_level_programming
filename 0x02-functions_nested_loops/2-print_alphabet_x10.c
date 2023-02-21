6cc790107e0c126885728404263cc39893668b98
islam-ahmed33 <eslamaborayah@gmail.com>
2023-02-21 14:32:47 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *print_alphabet - print all char from a to z 10x
+ *print_alphabet_x10 - print all char from a to z 10x
  * Description: 'the program's description'
  */
 void print_alphabet_x10(void)
 {
-int i=0;
-while(i<10)
+int i = 0;
+while( i < 10 )
 {
 char ch = 'a';
 while (ch <= 'z')
