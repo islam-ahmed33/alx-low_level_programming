4c3c8897872cc4ab35eb58f48ef9e0682c1eef5e
islam-ahmed33 <eslamaborayah@gmail.com>
2023-02-21 14:22:01 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,13 @@
  * Description: function to print_alphabet
  * Return: Always 0 (Success).
  */
-
 void print_alphabet(void)
 {
-  char ch = 'a';
-  while (ch <= 'z')
-    {
-      _putchar(ch);
-      ch++;
-    }
-  _putchar('\n');
+char ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
 }
