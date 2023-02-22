12274df24e97140ce267a04b94db850a03fac13d
islam-ahmed33 <eslamaborayah@gmail.com>
2023-02-21 18:33:10 -0800

    13. Nature made

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 /**
- *
  * main - Entry point
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  *
  */
+
 int main(void)
 {
-  int i = 0, sum = 0;
-  while(i < 1024 && ((i % 3 == 0) || (i % 5 == 0)))
-    {
-      sum += i;
-    }
-  printf("%d\n", sum);
-  return (0);
+int i, sum = 0;
+for (i = 1; i < 1024; i++)
+{
+if (i % 5 == 0 || i % 3 == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
 }
