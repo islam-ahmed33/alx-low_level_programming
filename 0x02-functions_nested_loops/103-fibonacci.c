02a916e11fcb7711089570fb2abee63e31ca8be3
islam-ahmed33 <eslamaborayah@gmail.com>
2023-02-21 18:51:08 -0800

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, k, sum;
+i = 1;
+j = 2;
+sum = 2;
+while (j <= 4000000) {
+k = i + j;
+i = j;
+j = k;
+if (j % 2 == 0)
+{
+sum += j;
+}
+}
+printf("%lu\n", sum);
+return (0);
+}
