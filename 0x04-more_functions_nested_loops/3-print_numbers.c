510db5d3d5cc54591705af6a179dea9ed1146fb8
islam-ahmed33 <eslamaborayah@gmail.com>
2023-02-23 12:54:25 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_most_numbers- print all numbers from 0 to 9 without 2, 4
+ * Description: 'the program's description'
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i + '0');
+}
+_putchar('\n');
+}
