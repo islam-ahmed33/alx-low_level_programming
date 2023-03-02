7b97a60d2785b8d6a70ec1278db5505accfdb7a1
islam-ahmed33 <eslamaborayah@gmail.com>
2023-03-02 08:29:59 -0800

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncat - function is similar to the _strcat function,
+ * except that it will use at most n bytes from src;
+ * and src does not need to be null-terminated
+ * if it contains n or more bytes
+ * @dest: the pointer to append string at
+ * @src: the pointer to string to be appended
+ * @n :bytes from src
+ * Return: the pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+	{
+		int i = 0, siz_D = 0;
+
+		while (dest[siz_D] != '\0')
+		{
+			siz_D++;
+		}
+		while (i < n)
+		{
+			dest[siz_D + i] = src[i];
+			i++;
+		}
+		dest[siz_D + i] = '\0';
+		return (dest);
+	}
