cd1d435cc9a0997c2b095c1e149829322b2ad42d
islam-ahmed33 <eslamaborayah@gmail.com>
2023-03-02 08:57:47 -0800

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -21,9 +21,14 @@ char *_strncat(char *dest, char *src, int n)
 		}
 		while (i < n)
 		{
-			dest[siz_D + i] = src[i];
-			i++;
+			if (src[i] != '\0')
+			{
+				dest[siz_D + i] = src[i];
+			}
+			else
+				break;
+		i++;
 		}
-		dest[siz_D + i] = '\0';
+		dest[siz_D + i + 1] = '\0';
 		return (dest);
 	}
