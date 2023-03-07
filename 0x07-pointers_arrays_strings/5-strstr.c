31da15d6a814e68df7e979ef54dc09c9ffb38c5f
islam-ahmed33 <eslamaborayah@gmail.com>
2023-03-06 18:10:25 -0800

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - locate character in a string
+ * @haystack: char array string
+ * @needle: substring to look for
+ * Return: NULL if char not found, or
+ * pointer to the beginning of the located substring.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			return (haystack);
+		}
+
+		haystack++;
+	}
+	if (*haystack == *needle)
+	{
+		return (haystack);
+	}
+	return ('\0');
+}
