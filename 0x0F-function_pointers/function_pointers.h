1dca89ba653836841e6912381cace4ae1f3f6431
islam-ahmed33 <eslamaborayah@gmail.com>
2023-03-22 18:03:51 -0700

    T2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,12 @@
 #ifndef FUNCTION_PTR
 #define FUNCTION_PTR
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 
 #endif
