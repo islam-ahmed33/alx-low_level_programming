4519fdf8a49eb342c2ead75fd0c9677a51fcb530
islam-ahmed33 <eslamaborayah@gmail.com>
2023-03-22 17:52:33 -0700

    T 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints a name.
+ *
+ * @name: string to print
+ * @f: ponter to function
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNCTION_PTR
+#define FUNCTION_PTR
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
