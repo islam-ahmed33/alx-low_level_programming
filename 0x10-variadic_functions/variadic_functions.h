ef4d7a7c9ecb69775b51aa9fe6e557e2fcdf371d
islam-ahmed33 <eslamaborayah@gmail.com>
2023-03-23 18:30:43 -0700

    creat variadic func

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,25 @@
+#ifndef VAR_FUNC_H
+#define VAR_FUNC_H
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+/**
+ * struct op - operation
+ * @op: print type
+ * @func: function
+ *
+ * By: Islam Ahmed
+ */
+
+typedef struct op
+{
+	char op;
+	void (*func)(va_list);
+} op_t;
+#endif
