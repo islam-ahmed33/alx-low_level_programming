ce4595282011643f918782b807066cb89ecbe51e
islam-ahmed33 <eslamaborayah@gmail.com>
2023-04-10 18:01:52 -0700

    T0.1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_unit - function that converts a binary number to an unsigned int.
+ * binary_to_uint - converts a binary number to an unsigned int.
  *
  * @b: is pointing to a string of 0 and 1 chars.
  *
