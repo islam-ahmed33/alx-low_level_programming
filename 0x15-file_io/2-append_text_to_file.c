0af8dc62ee38ae423016f5f2891476af7bc0bbb8
islam-ahmed33 <eslamaborayah@gmail.com>
2023-04-14 19:43:58 -0700

    T2.1

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,11 +2,11 @@
 
 /**
  * append_text_to_file - appends text at the end of a file.
- * 
+ *
  * @filename: A pointer to the name of the file.
  * @text_content: The string to add to the end of the file.
  *
- * Return: 1 if success or -1. 
+ * Return: 1 if success or -1.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
